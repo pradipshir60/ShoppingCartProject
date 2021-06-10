f173838050a69f413a1a7b619a9539bab483f687
Pradip <pradipshirsath123@gmail.com>
2021-06-10 08:56:56 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/ExceptionUtils.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+public class ExceptionUtils {
+
+	public static Throwable getRootCause(Exception e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
